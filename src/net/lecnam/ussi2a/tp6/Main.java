32b1f4ffbeba416d5e0e0d7b4a9cfef5b63ed6af
Maxime Garnier <maxime.garnier.auditeur@lecnam.net>
2021-12-08 09:58:15 +0100

    Initial commit

---
+++ src/net/lecnam/ussi2a/tp6/Main.java
@@ -0,0 +1,8 @@
+package net.lecnam.ussi2a.tp6;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/net/lecnam/ussi2a/tp6/Main2.java
@@ -0,0 +1,8 @@
+package net.lecnam.ussi2a.tp6;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+    }
+}
