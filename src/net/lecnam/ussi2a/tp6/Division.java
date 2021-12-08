6429dc34b2c0d24fe3b2ff0b91d63985e4f67db3
Maxime Garnier <maxime.garnier.auditeur@lecnam.net>
2021-12-08 11:30:53 +0100

    Exercice 3

--- src/net/lecnam/ussi2a/tp6/Division.java
+++ src/net/lecnam/ussi2a/tp6/Division.java
@@ -1,5 +1,7 @@
 package net.lecnam.ussi2a.tp6;
 
+import java.io.IOException;
+
 public class Division {
 
     public double division1(int a, int b){
@@ -8,13 +10,35 @@ public class Division {
         return resultat;
     }
 
-    public double division2(int a, int b){
+
+
+    public double division2(int a, int b)  {
+        try{
+            return a / b;
+        } catch (ArithmeticException e){
+            System.out.println("Impossible de diviser par 0 !!!!");
+        }
+
+        return 0;
+    }
+
+
+    /*public Double division2(int a, int b){
        try{
-        return a / b;
+         int resultat =  a / b;
+         return (double) resultat;
        } catch (Exception e){
            System.out.println("Impossible de diviser par 0 !!!!");
        }
-        return 0;
+        return null;
+    }*/
+
+    public double division3(int a, int b) throws IOException{
+        try{
+            return a / b;
+        } catch (ArithmeticException e){
+            throw new IOException();
+        }
     }
 
 }
--- src/net/lecnam/ussi2a/tp6/Main.java
+++ src/net/lecnam/ussi2a/tp6/Main.java
@@ -1,23 +1,31 @@
 package net.lecnam.ussi2a.tp6;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void exercice1(){
         Division d = new Division();
-        System.out.println(d.division1(10, 0)); //Exception in thread "main" java.lang.ArithmeticException: / by zero
-        // at net.lecnam.ussi2a.tp6.Division.division1(Division.java:7)
-        //  at net.lecnam.ussi2a.tp6.Main.main(Main.java:9)
+        double res =d.division1(10, 0);
+        System.out.println(res);;                  //Exception in thread "main" java.lang.ArithmeticException: / by zero
+                                                   // at net.lecnam.ussi2a.tp6.Division.division1(Division.java:7)
+                                                   //  at net.lecnam.ussi2a.tp6.Main.main(Main.java:9)
     }
     public static void exercice2(){
         Division d = new Division();
-        System.out.println(d.division2(10, 0)); //Exception in thread "main" java.lang.ArithmeticException: / by zero
-        // at net.lecnam.ussi2a.tp6.Division.division1(Division.java:7)
-        //  at net.lecnam.ussi2a.tp6.Main.main(Main.java:9)
+        double res =d.division2(10, 0);
+        System.out.println(res);
     }
 
-    public static void main(String[] args) {
+    public static void exercice3() throws IOException {
+        Division d = new Division();
+        double res =d.division3(10, 0);
+        System.out.println(res);
+    }
 
-        exercice2();
+    public static void main(String[] args) throws IOException {
+
+        exercice3();
 
     }
 }
